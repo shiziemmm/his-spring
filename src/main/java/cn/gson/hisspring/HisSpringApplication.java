de3c5c4a934238494628905839e5bb56c1c474c2
shizi <2054701360@qq.com>
2021-07-05 16:39:36 +0800

    Initial commit

---
+++ src/main/java/cn/gson/hisspring/HisSpringApplication.java
@@ -0,0 +1,13 @@
+package cn.gson.hisspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gson/hisspring/HisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gson.hisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
