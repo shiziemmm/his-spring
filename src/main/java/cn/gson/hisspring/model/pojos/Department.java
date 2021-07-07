dbf64fff610ab6acae66345f251f2218d395d3f1
shizi <2054701360@qq.com>
2021-07-07 09:30:42 +0800

    修改了实体类里面的科室和新增一个部门实体类

--- src/main/java/cn/gson/hisspring/model/pojos/Department.java
+++ src/main/java/cn/gson/hisspring/model/pojos/Department.java
@@ -3,11 +3,11 @@ package cn.gson.hisspring.model.pojos;
 import lombok.Data;
 
 @Data//get  set方法
-public class DepartmentKs {
+public class Department {
 
-  private long ksId;
-  private String ksName;
   private long deId;
+  private String deName;
+  private java.sql.Timestamp deDate;
 
 
 
