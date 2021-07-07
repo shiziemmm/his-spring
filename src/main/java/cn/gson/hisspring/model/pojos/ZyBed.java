b8440fd1a8bf90f5a45be040f5e2141dd34f59c4
shizi <2054701360@qq.com>
2021-07-07 15:01:17 +0800

    hebing

--- src/main/java/cn/gson/hisspring/model/pojos/ZyBed.java
+++ src/main/java/cn/gson/hisspring/model/pojos/ZyBed.java
@@ -2,15 +2,26 @@ package cn.gson.hisspring.model.pojos;
 
 import lombok.Data;
 
+
+/**
+ * 病床实体类
+ */
 @Data//get  set方法
 public class ZyBed {
 
-  private long bdId;
-  private String bdName;
-  private String wdId;
-  private double bdPrice;
-  private long bdIs;
-  private long ptNo;
+  private long bdId;//病床编号
+  private String bdName;//病床名称
+  private long wdId;//连接病房编号
+  private double bdPrice;//病床价格
+  private long bdIs;//是否能入住1表示可用2表示不可用需要维修 3表示已有病人入住
+  private long ptNo;//连接病人编号
+
+  //关系
+  private ZyWard ward;//病房实体类
+  private ZyPatientBase zyPatientBase;//住院登记实体类
+
+  //关系字段(方便查询这样就需要一个实体类)
+  private String ptName;//病人名称
 
 
 }
--- src/main/java/cn/gson/hisspring/model/pojos/ZyWard.java
+++ src/main/java/cn/gson/hisspring/model/pojos/ZyWard.java
@@ -1,14 +1,28 @@
 package cn.gson.hisspring.model.pojos;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
+import java.util.List;
+
+/**
+ * 病房实体类
+ */
 @Data//get  set方法
 public class ZyWard {
 
-  private long wdId;
-  private String wdName;
-  private String ksId;
-  private String sId;
+  private long wdId;//病房编号
+  private String wdName;//病房名称
+  private long ksId;//科室编号
+  private long sId;//员工编号
+
+  //关系
+  @TableField(exist = false)//这个注解表示该字段不是此表里面的字段
+  private List<ZyBed> listBed;//病床集合
+
+  @TableField(exist = false)//这个注解表示该字段不是此表里面的字段
+  private Staff staff;//员工对象
+
 
 
 }
