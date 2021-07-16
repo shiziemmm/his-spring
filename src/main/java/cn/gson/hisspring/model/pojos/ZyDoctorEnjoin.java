2f060d43ef0f8dfe62dacbf809e0cb87b9b9db5a
shizi <2054701360@qq.com>
2021-07-16 12:55:19 +0800

    更新住院登记以及住院申请

---
+++ src/main/java/cn/gson/hisspring/model/pojos/ZyDoctorEnjoin.java
@@ -0,0 +1,22 @@
+package cn.gson.hisspring.model.pojos;
+
+import lombok.Data;
+
+/**
+ * 医嘱表
+ */
+@Data
+public class ZyDoctorEnjoin {
+
+  private long deId;
+  private Long deLongorshort;
+  private Long ptNo;
+  private Long sId;
+  private String deDoctorName;
+  private java.sql.Timestamp deDate;
+  private Long deIs;
+
+
+
+
+}
