461a2379ca87d388238c51de86ccd9b4fd4d1ae3
shizi <2054701360@qq.com>
2021-07-23 08:28:28 +0800

    医嘱管理1.0

---
+++ src/main/java/cn/gson/hisspring/controller/inhospital_module_controller/DoctorEnjoinController.java
@@ -0,0 +1,15 @@
+package cn.gson.hisspring.controller.inhospital_module_controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 医嘱controller层
+ */
+@RestController
+@CrossOrigin
+public class DoctorEnjoinController {
+
+
+
+}
---
+++ src/main/java/cn/gson/hisspring/model/mapper/inhospital_module_mapper/DoctorEnjoinMapper.java
@@ -0,0 +1,13 @@
+package cn.gson.hisspring.model.mapper.inhospital_module_mapper;
+
+import cn.gson.hisspring.model.pojos.ZyDoctorEnjoin;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 医嘱mapper
+ */
+@Repository
+public interface DoctorEnjoinMapper extends BaseMapper<ZyDoctorEnjoin> {
+
+}
