475a0b2e9f4976da32812800318168b551029bb7
yinyou <1337374876@qq.com>
2021-07-27 11:48:57 +0800

    门诊问诊3

--- src/main/java/cn/gson/hisspring/model/pojos/MzZprescription.java
+++ src/main/java/cn/gson/hisspring/model/pojos/MzZprescription.java
@@ -1,16 +1,24 @@
 package cn.gson.hisspring.model.pojos;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+/**
+ *  中药处方-pojos
+ */
 
 @Data//get  set方法
 public class MzZprescription {
 
+  @TableId(type = IdType.AUTO)
   private long zpNumber;
+  private String zpName;
   private long drugId;
   private String zpSpecification;
   private long zpCount;
   private String zpUsage;
   private double zpPrice;
+  private String zpEntrust;
   private long recipeNumber;
 
 
