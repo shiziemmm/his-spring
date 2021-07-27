0045ec47300a2453468b55f264f42756ad9b4e7b
shizi <2054701360@qq.com>
2021-07-27 12:13:48 +0800

    7

--- src/main/java/cn/gson/hisspring/model/pojos/HcStockConsumables.java
+++ src/main/java/cn/gson/hisspring/model/pojos/HcStockConsumables.java
@@ -2,16 +2,13 @@ package cn.gson.hisspring.model.pojos;
 
 import lombok.Data;
 
-/**
- * 耗材库存表
- */
-@Data
+@Data//get  set方法
 public class HcStockConsumables {
 
-  private long stockConsumablesId;
+  private long consumablesStockId;
   private long consumablesId;
-  private long stockConsumablesNumber;
-
+  private long consumablesNumber;
+  private java.sql.Timestamp stockConsumableTime;
 
 
 }
