a96243aab77efa872f8a763051cfc7f006e55a3a
lenovo <874433126@qq.com>
2021-08-15 09:03:34 +0800

    排版显示ok

---
+++ src/main/java/cn/gson/hisspring/controller/jurisdiction_module_controller/FrequencyCategoryController.java
@@ -0,0 +1,92 @@
+package cn.gson.hisspring.controller.jurisdiction_module_controller;
+
+import cn.gson.hisspring.model.mapper.jurisdiction_module_mapper.SchedulingMapper;
+import cn.gson.hisspring.model.pojos.Frequency;
+import cn.gson.hisspring.model.pojos.FrequencyCategory;
+import cn.gson.hisspring.model.pojos.Scheduling;
+import cn.gson.hisspring.model.pojos.pojos_vo.WeekVo;
+import cn.gson.hisspring.model.service.jurisdiction_module_service.FrequencyCategoryService;
+import cn.gson.hisspring.model.service.jurisdiction_module_service.FrequencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class FrequencyCategoryController {
+    @Autowired
+    FrequencyCategoryService fre;
+    @Autowired
+    FrequencyService fs;
+    @Autowired
+    SchedulingMapper sl;
+    private static final int FIRST_DAY = Calendar.MONDAY;
+
+    @RequestMapping("list-fre")
+    public List<FrequencyCategory> dome(){
+        return fre.dome();
+    }
+
+    /**
+     * 查询班次信息
+     * @param bcId
+     * @return
+     */
+    @RequestMapping("bc-list")
+    public List<Frequency> dome1(long bcId){
+        List<Frequency> frequencies = fs.selectAllId(bcId);
+        return frequencies;
+    }
+    /**
+     * 根据科室查询人员排班信息
+     */
+
+    /*
+    获取当前星期
+     */
+    @RequestMapping("week")
+    public Object cs(long ksId){
+        List<WeekVo> ban2List=new ArrayList<>();
+        //获取当前时间
+        Calendar calendar = Calendar.getInstance();
+        //        调用方法回到本周的第一天
+        setToFirstDay(calendar);
+        for (int i = 0; i < 7; i++) {
+            WeekVo paiBan2=new WeekVo();
+//           将星期，日期，赋值
+            paiBan2 = printDay(calendar,paiBan2);
+            //查询这个科室的排班信息，并赋值
+            List<Scheduling> panBan = sl.selectWeek(paiBan2.getRq(),ksId);
+            paiBan2.setSlist(panBan);
+            //将查询出来的结果赋值给需要返回出去的集合
+            ban2List.add(paiBan2);
+            //System.out.println(rq);
+//           循环给日期加天数
+            calendar.add(Calendar.DATE, 1);
+        }
+       return ban2List;
+    }
+    private static void setToFirstDay(Calendar calendar) {
+//        获取本周的第一天
+        while (calendar.get(Calendar.DAY_OF_WEEK) !=FIRST_DAY) {
+            calendar.add(Calendar.DATE, -1);
+        }
+    }
+    private static WeekVo printDay(Calendar calendar,WeekVo weekVo) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat dateFormat2 = new SimpleDateFormat("EE");
+        weekVo.setXq(dateFormat2.format(calendar.getTime()));
+        weekVo.setRq(dateFormat.format(calendar.getTime()));
+        return weekVo;
+    }
+
+}
---
+++ src/main/java/cn/gson/hisspring/model/service/jurisdiction_module_service/FrequencyCategoryService.java
@@ -0,0 +1,17 @@
+package cn.gson.hisspring.model.service.jurisdiction_module_service;
+
+import cn.gson.hisspring.model.mapper.jurisdiction_module_mapper.FrequencyCategoryMapper;
+import cn.gson.hisspring.model.pojos.FrequencyCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FrequencyCategoryService {
+    @Autowired
+    FrequencyCategoryMapper fre;
+    public List<FrequencyCategory> dome(){
+        return fre.selectList(null);
+    }
+}
