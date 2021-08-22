617a2f4340eba3087ac41f91d50ee5b1444361d7
huiyan <2276007669@qq.com>
2021-08-22 19:31:51 +0800

    dqw手术模块1.0

--- src/main/java/cn/gson/hisspring/model/pojos/SsOperationApply.java
+++ src/main/java/cn/gson/hisspring/model/pojos/SsOperationApply.java
@@ -1,17 +1,26 @@
 package cn.gson.hisspring.model.pojos;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Data//get  set方法
 public class SsOperationApply {
-
+  @TableId(value = "apply_id",type = IdType.AUTO)
   private long applyId;
   private long ptNo;
   private String simulationOperation;
+  @JsonFormat(pattern = "yyyy-MM-dd",timezone="Asia/Shanghai")//将数据库的类型返回成指定类型
+  private java.sql.Timestamp simulationTime;
   private long sId;
   private long projectId;
   private long changeId;
   private long operationId;
 
-
+  @TableField(exist = false)
+  private ZyPatientBase ptdx;//病人信息
+  @TableField(exist = false)
+  private  Staff staff;//病人信息
 }
