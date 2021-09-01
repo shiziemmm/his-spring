0d4a440839700a30dc84c8469c66803664757aec
yinyou <1337374876@qq.com>
2021-09-01 11:15:47 +0800

    缴费3

---
+++ src/main/java/cn/gson/hisspring/model/pojos/pojos_vo/OverRecordVo.java
@@ -0,0 +1,18 @@
+package cn.gson.hisspring.model.pojos.pojos_vo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * 查询所有的就诊人员信息
+ */
+
+@Data
+@TableName("MzMedicalRecord")
+public class OverRecordVo {
+
+
+
+
+
+}
