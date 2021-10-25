180de3c0515b96aa311c4d44e5ddbd6d71b16060
lenovo <874433126@qq.com>
2021-10-25 11:38:15 +0800

    小雷工作计

--- src/main/java/cn/gson/hisspring/model/mapper/jurisdiction_module_mapper/SchedulingMapper.java
+++ src/main/java/cn/gson/hisspring/model/mapper/jurisdiction_module_mapper/SchedulingMapper.java
@@ -1,6 +1,7 @@
 package cn.gson.hisspring.model.mapper.jurisdiction_module_mapper;
 
 import cn.gson.hisspring.model.pojos.Scheduling;
+import cn.gson.hisspring.model.pojos.pojos_vo.ReportForm;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,7 +12,7 @@ import java.util.List;
 @Mapper
 public interface SchedulingMapper extends BaseMapper<Scheduling> {
     int addSch(@Param("rq") Date rq,@Param("sid") Integer sid, @Param("list") List<Integer> list);
-    List<Scheduling> selectAll(@Param("scDate") String star,@Param("scEnd") String end);
+//    List<Scheduling> selectAll(@Param("scDate") String star,@Param("scEnd") String end);
     List<Scheduling>selectWeek(@Param("rq")String rq,@Param("ksId") long ksId);
 
     List<Scheduling>selectAllWeek(@Param("rq")String rq);
@@ -19,5 +20,6 @@ public interface SchedulingMapper extends BaseMapper<Scheduling> {
     List<Scheduling>selectNow(@Param("rq")String rq,@Param("ksId") long ksId,@Param("sId") long sId);
     List<Scheduling> selectDate();
     List<Scheduling>seletSch(@Param("rq")String rq,@Param("list") List<Integer> dome);
+    List<Scheduling> selectKsName(ReportForm reportForm);
 
 }
